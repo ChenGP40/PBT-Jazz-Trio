5a3950cb337e8d8032c75841d24668e120d60ca7
ChenGP40 <byc.usa@gmail.com>
2025-07-25 17:01:33 -0400

    Big page update

---
+++ trio.js
@@ -0,0 +1,22 @@
+// gsap.timeline({ defaults: { opacity: 1, duration: 1, ease: "power1.in" } })
+//   .to("#landing1, #landing2, #landing3, #landing4", {})
+//   .to("#landing5", {})
+//   .to("#landing6", {})
+//   .to("#landing7", {})
+//   .to("#landing8", {})
+//   .to("#landingOverlay", {})
+//   .to("#landingText", {})
+// ;
+
+gsap.timeline({ defaults: { opacity: 1, duration: .75, ease: "power1.in" } })
+  .to("#landing1", {})
+  .to("#landing2", {})
+  .to("#landing3", {})
+  .to("#landing4", {})
+  .to("#landing5", {})
+  .to("#landing6", {})
+  .to("#landing7", {})
+  .to("#landing8", {})
+  .to("#landingOverlay", {})
+  .to("#landingText", {})
+;
